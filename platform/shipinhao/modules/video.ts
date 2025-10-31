d866a8f728079d8d48c665025a458d0b0ac0f3de
shenqicheng <1317225796@qq.com>
2025-10-31 17:45:12 +0800

    init

--- platform/shipinhao/modules/video.ts
+++ platform/shipinhao/modules/video.ts
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
-import RunnerContext from "../../RunnerContext.cjs";
-import { info } from "../../log.cjs";
+import RunnerContext from '../../../scripts/runner-context';
+import { info } from '../../../scripts/log';
 import type { Page, BrowserContext, ElementHandle } from 'playwright';
 
 interface PublishData {
